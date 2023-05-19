a12b4788369e1a1597309be8e78a92a4c0ce3253
yungifez <ifezueyoung@gmail.com>
2023-05-19 12:12:01 +0000

    made post page white

--- web/app/themes/gts-theme/resources/views/page.blade.php
+++ web/app/themes/gts-theme/resources/views/page.blade.php
@@ -1,8 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php(the_post())
-    @include('partials.page-header')
-    @includeFirst(['partials.content-page', 'partials.content'])
-  @endwhile
+  <div class="bg-white">
+
+    @while(have_posts()) @php(the_post())
+      @include('partials.page-header')
+      @includeFirst(['partials.content-page', 'partials.content'])
+    @endwhile
+  </div>
 @endsection
